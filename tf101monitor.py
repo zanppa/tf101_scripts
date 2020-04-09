5bc8f584a23cd554cb30a812f20824031976020e
Lauri Peltonen <30770072+zanppa@users.noreply.github.com>
2020-04-09 15:56:32 +0300

    Allow blank interface name in getproperty

--- tf101monitor.py
+++ tf101monitor.py
@@ -26,7 +26,7 @@ class Tf101Server(dbus.service.Object):
     # Property handlers
     @dbus.service.method(dbus.PROPERTIES_IFACE, in_signature='ss', out_signature='q')
     def Get(self, iface_name, prop_name):
-        if iface_name == 'org.tf101':
+        if iface_name == 'org.tf101' or iface_name == '':
             if prop_name == 'LidIsClosed':
                 return self.lid_status
         else:
